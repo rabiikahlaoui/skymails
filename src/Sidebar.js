c26eaf4ab8b9a7779a4bb706b76aa1a0479e313a
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2021-01-26 18:27:44 +0100

    Add SidebarOption component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
+import InboxIcon from "@mui/icons-material/Inbox";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import NoteIcon from "@mui/icons-material/Note";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 function Sidebar() {
   return (
@@ -12,6 +24,33 @@ function Sidebar() {
       >
         Compose
       </Button>
+
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" numbe r={54} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+          <div className="sidebar__footerIcons">
+            <IconButton>
+                <PersonIcon />
+            </IconButton>
+            <IconButton>
+                <DuoIcon />
+            </IconButton>
+            <IconButton>
+                <PhoneIcon />
+            </IconButton>
+          </div>
+      </div>
     </div>
   );
 }
